b21f6370dab880124dd8e277e1cb39ea876f8a51
unknown <asmamughal097@gmail.com>
2025-02-04 21:11:08 +0500

    commsion things added

--- routes/RideRoute.js
+++ routes/RideRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRide, deleteRide, getRideOffers, getRides, respondToRideOffer, updateRide, getDriverRideStats, getRidesByUser } from '../controllers/Rides/RideController.js';
+import { createRide, deleteRide, getRideOffers, getRides, respondToRideOffer, updateRide, getDriverRideStats, getRidesByUser, getDailyTotalAmount, getDailyTotalWithCommissionAndPayouts } from '../controllers/Rides/RideController.js';
 
 const router = express.Router();
 
@@ -11,4 +11,5 @@ router.get('/offers', getRideOffers);
 router.post('/respond', respondToRideOffer);
 router.get('/stats/:driverId', getDriverRideStats);
 router.get('/user-rides', getRidesByUser);
+router.get('/commission', getDailyTotalWithCommissionAndPayouts);
 export default router;
